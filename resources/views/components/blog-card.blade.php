78982c6a8e89b41e0090e26efd25daabc003012d
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2023-02-24 14:20:06 +0700

    Add Frontend

---
+++ resources/views/components/blog-card.blade.php
@@ -0,0 +1,29 @@
+<div class="col-sm-6 col-lg-4">
+    <div class="card card-overlay-bottom card-image-scale">
+        <!-- Card Image -->
+        <img src="{{ asset('frontend') }}/assets/images/blog/3by4/07.jpg" alt="">
+        <!-- Card Image overlay -->
+        <div class="card-img-overlay d-flex flex-column p-3 p-md-4">
+            <div>
+                <!-- Card category -->
+                <a href="#" class="badge text-bg-info mb-2"><i
+                        class="fas fa-circle me-2 small fw-bold"></i>Covid-19</a>
+            </div>
+            <div class="w-100 mt-auto">
+                <!-- Card title -->
+                <h4 class="text-white"><a href="post-single-5.html"
+                        class="btn-link text-reset stretched-link">Ten questions you should answer
+                        truthfully.</a></h4>
+                <!-- Card info -->
+                <ul class="nav nav-divider text-white-force align-items-center small">
+                    <li class="nav-item position-relative">
+                        <div class="nav-link">by <a href="#"
+                                class="stretched-link text-reset btn-link">Billy</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">Feb 22, 2022</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>

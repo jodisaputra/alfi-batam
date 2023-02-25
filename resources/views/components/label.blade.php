9253339875db07ed32d2ffb1d5fa28bba9156f6e
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2023-02-25 15:32:45 +0700

    Push Saja

---
+++ resources/views/components/label.blade.php
@@ -0,0 +1,1 @@
+<label for="{{ $label }}">{{ $slot }}</label>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\Backend\DashboardController;
 
 Route::get('/', [\App\Http\Controllers\frontend\HomeController::class, 'index']);
+Route::get('/login', function () {
+    echo 'Login';
+})->name('login');
+
+Route::prefix('admin')->group(function () {
+
+    //group login controller
+    Route::middleware('guest')->group(function () {
+        Route::controller(LoginController::class)->group(function () {
+            Route::get('login', 'showLoginForm')->name('admin.login.show');
+            Route::post('login', 'loginUser')->name('admin.login.post');
+        });
+    });
+    Route::post('logout', LogoutController::class)
+        ->middleware('auth')
+        ->name('admin.logout');
+    //end group login controller
+    Route::middleware('auth')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+    });
+});

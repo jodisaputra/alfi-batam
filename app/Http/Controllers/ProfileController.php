aea915826fd6cea6de16692abed243e4cfd12f7d
jodisaputra <116236799+jodisaputra@users.noreply.github.com>
2023-02-25 21:44:35 +0700

    Upload Profile Picture

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -21,10 +21,19 @@ class ProfileController extends Controller
     {
         $request->validate([
             'name' => ['required'],
+            'profile_picture' => ['nullable', 'mimes:jpg,jpeg,png'],
         ]);
         $user = User::findOrFail(Auth::user()->id)->first();
+
+        if($request->hasFile('profile_picture')){
+            $profileValue = $request->profile_picture;
+            $filename = time() . date('Y-m-d').$profileValue->getClientOriginalExtension();
+            $request->profile_picture->storeAs('user',$filename,'public');
+        }
         $user->update([
             'name' => $request->name,
+            'profile_picture' => $request->has('profile_picture') ?
+                $filename : $user->profile_picture
         ]);
         Alert::toast('Profile berhasil diubah', 'success');
         return redirect()->route('admin.profile');
--- resources/views/pages/auth-backend/profile.blade.php
+++ resources/views/pages/auth-backend/profile.blade.php
@@ -12,8 +12,13 @@
                 <x-card style="card-primary card-outline">
                     <x-card-body style="box-profile">
                         <div class="text-center">
+                            @if ($user->profile_picture == null)
                             <img class="profile-user-img img-fluid img-circle"
                                 src="{{ asset('backend') }}/dist/img/user4-128x128.jpg" alt="User profile picture">
+                            @else
+                            <img class="profile-user-img img-fluid img-circle"
+                                src="{{ asset('storage/user/' . $user->profile_picture) }}" alt="User profile picture">
+                            @endif
                         </div>
                         <h3 class="profile-username text-center">{{ Auth::user()->name }}</h3>
                     </x-card-body>
@@ -29,24 +34,36 @@
                     </div><!-- /.card-header -->
                     <x-card-body>
                         <div class="tab-content">
-                            <form class="form-horizontal" method="post" action="{{ route('admin.profile.update') }}">
+                            <form enctype="multipart/form-data" class="form-horizontal" method="post" action="{{ route('admin.profile.update') }}">
                                 @csrf
                                 @method('PUT')
                                 <div class="form-group row">
                                     <x-label label="name" style="col-sm-2 col-form-label">Nama</x-label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                            name="name" id="inputName" placeholder="Nama" value="{{ old('name', $user->name) }}">
+                                            name="name" id="inputName" placeholder="Nama"
+                                            value="{{ old('name', $user->name) }}">
                                         @error('name')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                    <x-label label="email" style="col-sm-2 col-form-label">Email</x-label>
                                     <div class="col-sm-10">
-                                        <input readonly type="text" class="form-control" name="email" id="inputEmail"
+                                        <input readonly type="text" class="form-control" name="email" id="email"
                                             placeholder="Email" value="{{ $user->email }}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <x-label label="profile_picture" style="col-sm-2 col-form-label">Foto Profil <small>Silahkan kosongkan jika tidak ingin mengganti foto profil</small></x-label>
+                                    <div class="col-sm-10">
+                                        <input type="file"
+                                            class="form-control @error('profile_picture') is-invalid @enderror"
+                                            name="profile_picture" id="profile_picture">
+                                        @error('profile_picture')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
                                     </div>
                                 </div>
                                 <div class="form-group row">
